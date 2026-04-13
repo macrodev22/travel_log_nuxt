c6d21cc3db0be7219a8d7ff9fd929d14d97a7b05
Ivan Sempebwa <iproivan@gmail.com>
2026-04-13 21:32:07 +0300

    add landing page

--- app/app.vue
+++ app/app.vue
@@ -1,8 +1,5 @@
 <template>
-  <h3 class="text-2xl text-amber-500">
-    Hello from Tailwind
-  </h3>
-  <button class="btn btn-secondary">
-    Test Daisy
-  </button>
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
 </template>
