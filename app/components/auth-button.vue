0b33758ad5bce959ebb3f7715f78f90144198bf3
= <=>
2026-04-18 16:38:25 +0300

    add auth and auth schema, plus auth button component

---
+++ app/components/auth-button.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { createAuthClient } from "better-auth/client";
+
+const { accent } = defineProps({ accent: { type: Boolean, default: false } });
+
+const authClient = createAuthClient();
+
+async function signIn() {
+  await authClient.signIn.social({
+    provider: "github",
+    callbackURL: "/dashboard",
+  });
+}
+</script>
+
+<template>
+  <button
+    class="btn"
+    :class="{ 'btn-accent': accent }"
+    @click="signIn"
+  >
+    Sign In with Github <Icon name="tabler:brand-github" />
+  </button>
+</template>
--- lib/auth.ts
+++ lib/auth.ts
@@ -1,7 +1,14 @@
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import db from "./db";
+import env from "./env";
 
 export const auth = betterAuth({
   database: drizzleAdapter(db, { provider: "sqlite" }),
+  socialProviders: {
+    github: {
+      clientId: env.GITHUB_CLIENT_ID as string,
+      clientSecret: env.GITHUB_CLIENT_SECRET as string,
+    },
+  },
 });
