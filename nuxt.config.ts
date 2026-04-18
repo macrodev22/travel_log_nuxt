8111bbda629ea8a04b519a7717f5e1c8ffeedcff
macrodev22 <iproivan@gmail.com>
2026-04-18 21:27:06 +0300

    fix auth config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,7 +6,12 @@ import "./lib/env";
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
-  modules: ["@nuxt/eslint", "@nuxt/icon", "@nuxtjs/color-mode"],
+  modules: [
+    "@nuxt/eslint",
+    "@nuxt/icon",
+    "@nuxtjs/color-mode",
+    "@pinia/nuxt",
+  ],
   css: ["./app/assets/css/main.css"],
   eslint: {
     config: { standalone: false },
@@ -18,5 +23,8 @@ export default defineNuxtConfig({
   },
   colorMode: {
     dataValue: "theme",
+  },
+  pinia: {
+    storesDirs: ["./app/stores"],
   },
 });
