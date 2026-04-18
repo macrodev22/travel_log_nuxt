6643a1b24575f528e9ad766cc774cdc5d4ebe959
macrodev22 <iproivan@gmail.com>
2026-04-18 20:38:54 +0300

    fix wrong turso auth connector

--- lib/auth.ts
+++ lib/auth.ts
@@ -2,9 +2,10 @@ import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import db from "./db";
 import env from "./env";
+import * as schema from "./db/schema/auth-schema";
 
 export const auth = betterAuth({
-  database: drizzleAdapter(db, { provider: "sqlite" }),
+  database: drizzleAdapter(db, { provider: "sqlite", schema }),
   socialProviders: {
     github: {
       clientId: env.GITHUB_CLIENT_ID as string,
--- lib/db/migrations/20260416164732_tan_iron_man/migration.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE `location` ADD `user_id` text NOT NULL REFERENCES user(id);--> statement-breakpoint
-ALTER TABLE `location_log` ADD `user_id` text NOT NULL REFERENCES user(id);--> statement-breakpoint
-ALTER TABLE `location_log_image` ADD `user_id` text NOT NULL REFERENCES user(id);
